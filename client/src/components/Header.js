7b09e44c10a37cb54ee6b15e246e6e7e3fe30fb6
sebastiangol <sebastiangolshani@yahoo.co.uk>
2022-02-18 00:38:34 +0000

    TailwindCSS Setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      Boss Tracker Home Page
+    </div>
+  );
+}
+
+export default Home;
